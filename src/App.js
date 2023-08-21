968bae815c939348bb0380ca097db44d7df15799
K-Sikaria <khushisikaria1@gmail.com>
2023-08-21 09:26:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css'
+import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
+import Features from './Components/Features/Features'
+import AboutUs from './Components/AboutUs/AboutUs'
+import Footer from './Components/Footer/Footer'
+
+const App = () => {
+  return (
+    <>
+    <Navbar />
+    {/*<Home />
+    <Features />
+    <AboutUs />
+  <Footer />*/}
+    </>
+  )
+}
+
+export default App
